b4a1775ead028a773a2a3f6b00aaf3ce39dc8288
DFernsCap4 <142505558+DFernsCap4@users.noreply.github.com>
2024-04-11 17:09:56 +0200

    test os to access .env

--- database/firebase.py
+++ database/firebase.py
@@ -1,17 +1,26 @@
+import os
 import json
 import firebase_admin
 from firebase_admin import credentials
-import pyrebase
-import os
+import pyrebase 
 
-from dotenv import dotenv_values
+# Get environment variables from the system environment
+firebase_service_account_key_str = os.environ.get('FIREBASE_SERVICE_ACCOUNT_KEY')
+firebase_config_str = os.environ.get('FIREBASE_CONFIG')
 
-config = dotenv_values(".env")
+# Make sure to check if the environment variables were actually retrieved
+if not firebase_service_account_key_str or not firebase_config_str:
+    raise ValueError("The necessary environment variables were not found.")
+
+# Parse the service account key and firebase config from the environment variable strings
+cred_dict = json.loads(firebase_service_account_key_str)
+firebase_config_dict = json.loads(firebase_config_str)
 
 # Initialize Firebase Admin with the service account information
-cred = credentials.Certificate(json.loads(os.getenv('FIREBASE_SERVICE_ACCOUNT_KEY')))
+cred = credentials.Certificate(cred_dict)
 firebase_admin.initialize_app(cred)
 
-firebase = pyrebase.initialize_app(json.loads(os.getenv('FIREBASE_CONFIG')))
+# Initialize Pyrebase with the config information
+firebase = pyrebase.initialize_app(firebase_config_dict)
 db = firebase.database()
 authSession = firebase.auth()
