41c75d23044812f6835017aac6791a6673d10fe5
DFernsCap4 <142505558+DFernsCap4@users.noreply.github.com>
2024-04-11 17:03:02 +0200

    test os to access .env

--- database/firebase.py
+++ database/firebase.py
@@ -1,16 +1,17 @@
 import json
 import firebase_admin
 from firebase_admin import credentials
-import pyrebase 
+import pyrebase
+import os
 
 from dotenv import dotenv_values
 
 config = dotenv_values(".env")
 
 # Initialize Firebase Admin with the service account information
-cred = credentials.Certificate(json.loads(config['FIREBASE_SERVICE_ACCOUNT_KEY']))
+cred = credentials.Certificate(json.loads(os.getenv('FIREBASE_SERVICE_ACCOUNT_KEY')))
 firebase_admin.initialize_app(cred)
 
-firebase = pyrebase.initialize_app(json.loads(config['FIREBASE_CONFIG']))
+firebase = pyrebase.initialize_app(json.loads(os.getenv('FIREBASE_CONFIG')))
 db = firebase.database()
 authSession = firebase.auth()
