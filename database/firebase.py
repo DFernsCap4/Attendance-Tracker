9f4a89941f8ef849ffdceeec93b04227dab8f751
Cwlass <142505558+DFernsCap4@users.noreply.github.com>
2024-04-12 10:39:38 +0200

    Corrected .env reference to new structure allowing environement variables at os level

--- database/firebase.py
+++ database/firebase.py
@@ -2,10 +2,15 @@ import json
 import firebase_admin
 from firebase_admin import credentials
 import pyrebase 
+import os
 
-from dotenv import dotenv_values
+from dotenv import load_dotenv
 
-config = dotenv_values(".env")
+load_dotenv()
+
+config={
+    "FIREBASE_SERVICE_ACCOUNT_KEY": os.getenv("FIREBASE_SERVICE_ACCOUNT_KEY")
+}
 
 # Initialize Firebase Admin with the service account information
 cred = credentials.Certificate(json.loads(config['FIREBASE_SERVICE_ACCOUNT_KEY']))
