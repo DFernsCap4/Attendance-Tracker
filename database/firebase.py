dd54ab92d420b969db2b700052a3188fcde1d71b
Cwlass <142505558+DFernsCap4@users.noreply.github.com>
2024-04-12 14:18:54 +0200

    Added coverage configuration

--- database/firebase.py
+++ database/firebase.py
@@ -12,7 +12,6 @@ config={
     "FIREBASE_SERVICE_ACCOUNT_KEY": os.getenv("FIREBASE_SERVICE_ACCOUNT_KEY"),
     "FIREBASE_CONFIG": os.getenv("FIREBASE_CONFIG")
 }
-
 # Initialize Firebase Admin with the service account information
 cred = credentials.Certificate(json.loads(config['FIREBASE_SERVICE_ACCOUNT_KEY']))
 firebase_admin.initialize_app(cred)
